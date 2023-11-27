9dbf72bc3faaf3e279ef86c8f41b21c80f592201
chuckleberryfinn <chuckleberryfinn@gmail.com>
2023-11-27 16:02:52 +0000

    AoC-2023 initial commit

---
+++ src/bin/day01.rs
@@ -0,0 +1,17 @@
+fn main() {
+    println!("AoC-2023");
+    println!("AoC-2023");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+    }
+
+    #[test]
+    fn test_part2() {
+    }
+}

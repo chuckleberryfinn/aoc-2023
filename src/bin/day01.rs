f3b8f3a5ac978f8d12bfda0eb852bc954e72f4df
chuckleberryfinn <chuckleberryfinn@gmail.com>
2023-11-28 05:41:25 +0000

    Cleanup

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,6 +1,15 @@
 fn main() {
+    part1();
+    part2();
     println!("AoC-2023");
-    println!("AoC-2023");
+}
+
+fn part1() -> u8 {
+    1
+}
+
+fn part2() -> u8 {
+    1
 }
 
 #[cfg(test)]
@@ -9,9 +18,11 @@ mod tests {
 
     #[test]
     fn test_part1() {
+        assert!(part1() == 1);
     }
 
     #[test]
     fn test_part2() {
+        assert!(part2() == 1);
     }
 }

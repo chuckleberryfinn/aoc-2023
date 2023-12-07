a3b0959ef8e1e76d9a72a7b6d47cdf8987f697b8
chuckleberryfinn <chuckleberryfinn@gmail.com>
2023-12-07 14:47:40 +0000

    More cleanup

--- src/bin/day07.rs
+++ src/bin/day07.rs
@@ -14,16 +14,6 @@ enum CamelHand {
     FiveOfAKind,
 }
 
-fn get_input() -> Vec<(&'static str, u32)> {
-    include_str!("../../input/day07.txt")
-        .lines()
-        .map(|l| l.split_once(' ').unwrap())
-        .collect::<Vec<(&'static str, &'static str)>>()
-        .iter()
-        .map(|(hand, score)| (*hand, score.parse().unwrap()))
-        .collect::<Vec<(&'static str, u32)>>()
-}
-
 fn card_ordering(card: char, jokers_wild: bool) -> usize {
     match card {
         'A' => 14,
@@ -46,19 +36,35 @@ fn card_ordering(card: char, jokers_wild: bool) -> usize {
     }
 }
 
-fn compare_cards(hand1: &'static str, hand2: &'static str, jokers_wild: bool) -> Ordering {
-    hand1
-        .chars()
-        .zip(hand2.chars())
-        .find_map(|(h1, h2)| {
-            let order_h1 = card_ordering(h1, jokers_wild);
-            let order_h2 = card_ordering(h2, jokers_wild);
-            match order_h1.cmp(&order_h2) {
-                Ordering::Equal => None,
-                order => Some(order),
-            }
-        })
-        .unwrap_or(Ordering::Equal)
+fn main() {
+    println!("{}", part1());
+    println!("{}", part2());
+}
+
+fn part1() -> u32 {
+    get_winnings(false)
+}
+
+fn part2() -> u32 {
+    get_winnings(true)
+}
+
+fn get_winnings(jokers_wild: bool) -> u32 {
+    get_input()
+        .iter()
+        .sorted_by(|a, b| compare_hand(a.0, b.0, jokers_wild))
+        .zip(1..)
+        .fold(0, |acc, (v, i)| acc + (v.1 * i))
+}
+
+fn get_input() -> Vec<(&'static str, u32)> {
+    include_str!("../../input/day07.txt")
+        .lines()
+        .map(|l| l.split_once(' ').unwrap())
+        .collect::<Vec<(&'static str, &'static str)>>()
+        .iter()
+        .map(|(hand, score)| (*hand, score.parse().unwrap()))
+        .collect::<Vec<(&'static str, u32)>>()
 }
 
 fn compare_hand(hand1: &'static str, hand2: &'static str, jokers_wild: bool) -> Ordering {
@@ -86,29 +92,26 @@ fn evaluate_hand(hand: &str, jokers_wild: bool) -> CamelHand {
             .map(|(k, _)| *k)
             .collect::<Vec<char>>();
         if sorted[0] != 'J' {
-            *cards.entry(sorted[0]).or_default() += *cards.get(&'J').unwrap_or(&0);
-            cards.remove(&'J');
+            *cards.get_mut(&sorted[0]).unwrap() += cards.remove(&'J').unwrap_or(0);
         } else if sorted.len() > 1 {
-            *cards.entry('J').or_default() += *cards.get(&sorted[1]).unwrap();
-            cards.remove(&sorted[1]);
+            *cards.get_mut(&'J').unwrap() += cards.remove(&sorted[1]).unwrap();
         }
     }
 
-    let keys: Vec<&char> = cards.keys().collect();
-    let values: Vec<&u8> = cards.values().collect();
+    let items: Vec<(&char, &u8)> = cards.iter().collect();
 
-    match keys.len() {
+    match items.len() {
         1 => CamelHand::FiveOfAKind,
-        2 => match values[0] {
+        2 => match items[0].1 {
             1 | 4 => CamelHand::FourOfAKind,
             _ => CamelHand::FullHouse,
         },
-        3 => match values[0] {
-            2 => CamelHand::TwoPair,
-            1 => match values[1] {
+        3 => match items[0].1 {
+            1 => match items[1].1 {
                 2 => CamelHand::TwoPair,
                 _ => CamelHand::ThreeOfAKind,
             },
+            2 => CamelHand::TwoPair,
             _ => CamelHand::ThreeOfAKind,
         },
         4 => CamelHand::OnePair,
@@ -116,25 +119,19 @@ fn evaluate_hand(hand: &str, jokers_wild: bool) -> CamelHand {
     }
 }
 
-fn get_winnings(jokers_wild: bool) -> u32 {
-    get_input()
-        .iter()
-        .sorted_by(|a, b| compare_hand(a.0, b.0, jokers_wild))
-        .zip(1..)
-        .fold(0, |acc, (v, i)| acc + (v.1 * i))
-}
-
-fn part1() -> u32 {
-    get_winnings(false)
-}
-
-fn part2() -> u32 {
-    get_winnings(true)
-}
-
-fn main() {
-    println!("{}", part1());
-    println!("{}", part2());
+fn compare_cards(hand1: &'static str, hand2: &'static str, jokers_wild: bool) -> Ordering {
+    hand1
+        .chars()
+        .zip(hand2.chars())
+        .find_map(|(h1, h2)| {
+            let order_h1 = card_ordering(h1, jokers_wild);
+            let order_h2 = card_ordering(h2, jokers_wild);
+            match order_h1.cmp(&order_h2) {
+                Ordering::Equal => None,
+                order => Some(order),
+            }
+        })
+        .unwrap_or(Ordering::Equal)
 }
 
 #[cfg(test)]
